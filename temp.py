549ad4fb4e595a6e6fe79aa67e7d68850ed6cfc8
AlondraMin <31377903+AlondraMin@users.noreply.github.com>
2017-09-04 13:19:22 -0500

    Add files via upload
    Ejercicios cambios completo

--- temp.py
+++ temp.py
@@ -5,7 +5,7 @@ x=[1997,1998,1999,2000,2001,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,20
 #Crea un vector (arreglo) con valores en el eje Y para cada valor en el eje X
 y=[0,1,1,1,3,3,3,3,4,4,4,4,4,4,4,4,4,5,5,5,5,5]
 #Grafica del vector x contra el vector y
-pl.plot(x,y)
+pl.plot(x,y, 'pm')
 pl.ylabel('Numero de novios y mascotas')
 pl.xlabel('Anios')
 pl.title('Numero de novios y mascotas por anios de vida')
